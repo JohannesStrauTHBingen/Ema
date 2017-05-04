091a9fcfb1b38295220de2a0aeba640fea882e3a
Student <student@SS17vmac16.local>
2017-05-04 11:11:25 +0200

    Blatt 3 Aufgabe 1

---
+++ JohannesStrauss Uebung3/Aufgabe1.playground/Contents.swift
@@ -0,0 +1,112 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Person{
+    
+    private var vorname : String
+    private var nachname : String
+    
+    init(vorname : String, nachname : String) {
+        self.vorname = vorname
+        self.nachname = nachname
+    }
+    
+    func getVorname() -> String{
+        return self.vorname
+    }
+    
+    func getNachName() -> String{
+        return self.nachname
+    }
+    
+    func toString() -> String{
+        return self.getVorname() + " " + self.getNachName()
+    }
+    
+    func printPerson(){
+        print(self.getVorname() + " " + self.getNachName())
+    }
+}
+
+class Spieler : Person {
+    
+    private var trikoNr : Int
+    private var position : Position
+    
+    init(vorname : String, nachname : String , trikoNr : Int, position : Position) {
+
+        self.trikoNr = trikoNr
+        self.position = position
+        
+        super.init(vorname : vorname, nachname : nachname)
+        
+
+    }
+    
+    func getTrikoNr() -> Int{
+        return self.trikoNr
+    }
+    
+    func getPosition() -> Position {
+        return self.position
+    }
+    
+    func printSpieler() {
+        print("Nr. " + String(self.trikoNr) + ", " + super.toString() + ", Position: " + self.getPosition().rawValue)
+    }
+}
+
+enum Position : String {
+    
+    case Sturm = "Sturm"
+    case Mittelfeld = "Mittelfeld"
+    case Abwehr = "Abwehr"
+    case Tor = "Tor"
+}
+
+class Mannschaft{
+
+    var name : String
+    var torwart : Spieler
+    var feldSpieler : [Spieler]
+    var tabellenPlatz : Int
+    
+    init(name : String, torwart : Spieler , feldSpieler : [Spieler] , tabellenPlatz : Int) {
+        
+        self.name = name
+        self.torwart = torwart
+        self.feldSpieler = feldSpieler
+        self.tabellenPlatz = tabellenPlatz
+    }
+    
+    func getName() -> String{
+    return self.name;
+    }
+    
+    func gewinntSpielt() -> Bool{
+        return arc4random_uniform(2) == 0
+    }
+    
+    func printMannschaft() {
+        print(self.getName())
+        torwart.printSpieler()
+        
+        for spieler in feldSpieler {
+            spieler.printSpieler()
+        }
+        print("Tabellenplatz: " + String(tabellenPlatz))
+    }
+}
+
+var towart = Spieler(vorname : "Manuel", nachname :  "Neuer", trikoNr :  1, position : Position.Tor)
+
+var feld = [Spieler(vorname: "Sepp", nachname: "Mayer", trikoNr: 5, position: Position.Mittelfeld)]
+feld.append(Spieler(vorname: "Thomas", nachname: "Müller", trikoNr: 7, position: Position.Mittelfeld))
+feld.append(Spieler(vorname: "Gerd", nachname: "Müller", trikoNr: 3, position: Position.Sturm))
+
+var bayern = Mannschaft(name: "Bayern", torwart: towart, feldSpieler: feld, tabellenPlatz: 5)
+
+bayern.printMannschaft()
