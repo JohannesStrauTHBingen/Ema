c918eeb2ffcab8e4fbfe5fc0b07522b8c6c8f48a
Student <student@SS17vmac16.local>
2017-05-11 20:31:36 +0200

    Hello Git

---
+++ JohannesStraussUebung4/HelloGit/HelloGit/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloGit
+//
+//  Created by Student on 11.05.17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello DidLoads")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+
+}
+
